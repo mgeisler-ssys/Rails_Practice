c6c0f8f24798b99b40e485f837ecb8554991a45c
mgeisler-ssys <max.geisler@stratasys-inc.biz>
2015-06-28 16:46:22 -0500

    Validation Exercise

--- RailsPractice_depot/depot/app/models/product.rb
+++ RailsPractice_depot/depot/app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price , numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-
+  validates_length_of :description, minimum: 10, message: "Description should be at least 10 chars."
   validates :image_url, allow_blank:
       true, format: {
     with: %r{\.(gif|jpg|png)\Z}i, 
